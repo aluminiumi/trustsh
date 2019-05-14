492547f6b56b574e579991001258911a9f0c2ebf
Sasha Romanov <22565619+aluminiumi@users.noreply.github.com>
2019-05-14 15:23:56 -0400

    argument handling completed

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ use libc::{close, dup2, execvp, fork};
 use std::env;
 use std::io;
 use std::io::{stdout, Write};
+use std::process;
+
+const EXIT_ERROR: i32 = 1;
 
 fn main() {
+    // initialize variables
+    let mut disable_prompt = false;
+    let mut verbose = false;
+
     // redirect stderr to stdout
     unsafe {
         dup2(libc::STDOUT_FILENO, libc::STDERR_FILENO);
@@ -11,24 +18,42 @@ fn main() {
 
     // parse command line
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    match &args[1][..] {
-        "-h" => println!("help"),
-        "-v" => println!("verbose"),
-        "-p" => println!("prompt"),
-        _ => println!("unknown argument"),
+    // println!("{:?}", args);
+    if args.len() == 2 {
+        match &args[1][..] {
+            "-h" => usage(),
+            "-v" => verbose = true,
+            "-p" => disable_prompt = true,
+            _ => usage(),
+        }
     }
 
     // proceed to infinite execution loop
-    main_loop();
+    main_loop(disable_prompt, verbose);
 }
 
-fn main_loop() {
+fn usage() {
+    println!("Usage: trustsh [-hvp]\n");
+    println!("   -h   prints this message\n");
+    println!("   -v   prints additional debug information\n");
+    println!("   -p   disable display of command prompt\n");
+    process::exit(EXIT_ERROR);
+
+}
+
+fn main_loop(disable_prompt: bool, verbose: bool) {
+    if verbose {
+        println!("verbose mode enabled");
+    }
+
     loop {
         // print prompt
-        print!("$ ");
-        stdout().flush()
-            .expect("Failed to flush prompt");
+        if !disable_prompt {
+            print!("$ ");
+            stdout().flush()
+                .expect("Failed to flush prompt");
+        }
+
 
         // feed user input into string
         let mut cmdline = String::new();
