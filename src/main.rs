a42c352d3c888524f9ead528de4e7c4f440cd968
Sasha Romanov <22565619+aluminiumi@users.noreply.github.com>
2019-05-14 14:11:58 -0400

    prompt on single line, handle EOF, and added eval function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 use std::io;
+use std::io::stdout;
+use std::io::Write;
 
 fn main() {
     loop {
         // print prompt
-        println!("$ ");
+        print!("$ ");
+        stdout().flush()
+            .expect("Failed to flush prompt");
 
+        // feed user input into string
         let mut cmdline = String::new();
         io::stdin().read_line(&mut cmdline)
             .expect("Failure reading command line");
 
-        println!("Command was {}", cmdline);
+        // handle EOF/Ctrl-D
+        if cmdline.len() == 0 {
+            break;
+        }
+
+        eval(cmdline);
     }
+}
+
+fn eval(cmdline: String) {
+    // repeat command to user
+    println!("Command was {}", cmdline);
+
 }
